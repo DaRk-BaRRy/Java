599929026370667d7b3a67c5feb14d416e796f55
DaRkBaRRy <DaRk-BaRRy>
2023-03-19 10:45:25 +0900

    JAva

---
+++ Project/src/P2/Calss.java
@@ -0,0 +1,22 @@
+package P2;
+
+public class Calss {
+public static void main(String[] args) {
+	//java
+	
+	
+	
+	System.out.println();
+	
+	String wasans = "WA SANS";
+	System.out.printf("%s\n", wasans);
+	
+	System.out.println(3/3);
+	System.out.println(5*8);
+	System.out.println(3.0/48448.5);
+	System.out.println(1/3.0);
+	System.out.println(3&3);
+	System.out.println((int)1.0);
+	System.out.println(1.0);
+}
+}
