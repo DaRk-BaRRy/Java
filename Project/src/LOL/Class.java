8cec2fcc85fa1ef6e499cf6d2a7d9e38cab41376
DaRkBaRRy <DaRk-BaRRy>
2023-04-29 13:17:39 +0900

    JAva

--- Project/src/LOL/Class.java
+++ Project/src/LOL/Class.java
@@ -1,5 +1,23 @@
 package LOL;
 
-public class Class {
+import java.util.*;
 
-}
+public class Class {
+public static void main(String[] args) {
+	String menu[] = {null, null, "wasans"};
+	
+	ArrayList<String> A1 = new ArrayList<>();
+	A1.add("wasans");
+	System.out.println(A1.get(0));
+	A1.set(0, "papyrus");
+	System.out.println(A1.get(0));
+	System.out.println(A1.size());
+	A1.remove(0);
+	A1.add("REPLICATED");
+	System.out.println(A1.indexOf("REPLICATED"));
+	
+	
+	
+	
+	
+}}
